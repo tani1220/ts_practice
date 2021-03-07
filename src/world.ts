cfeba2482b20985ed6ec8fead4969c5abd68690a
tani <berukoko1220-blog@yahoo.co.jp>
2021-03-07 20:27:17 +0900

    fix husky

--- src/world.ts
+++ src/world.ts
@@ -6,12 +6,9 @@ export default class World {
     this.message = message;
   }
 
-  public sayHello(elem: HTMLElement | null){
+  public sayHello(elem: HTMLElement | null): void {
     if (elem) {
       elem.innerText = this.message;
     }
-
-
-    
   }
 }
